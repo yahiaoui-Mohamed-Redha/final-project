6b731fdf9dae2b10c380b4fe7bf163c045e6f57e
Mohamed-Redha <yahiaouimohamedredha@gmail.com>
2025-02-10 11:57:06 +0100

    Saved my local changes before pulling

--- form-sass/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destroy the session
-header("Location: login.php"); // Redirect to login page
-exit();
-?>
--- form-sass/save_form.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $formData = $_POST['formData'];
-    file_put_contents('saved_form.txt', $formData);
-    echo "Form saved successfully!";
-}
