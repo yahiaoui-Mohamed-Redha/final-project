1405edf0ea759fb7cd95321a9c91c1d2cda59d68
RAOUF <abdraoufbelabes@gmail.com>
2025-03-14 16:32:27 +0100

    v-1.2.0

--- dist/gerer_les_ordres_des_missions/gerer_ord.php
+++ dist/gerer_les_ordres_des_missions/gerer_ord.php
@@ -35,7 +35,26 @@ $admin = $select->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <section class="form-container">
-    <h1>Order Missions</h1>
+<div class="w-full bg-white flex items-center justify-between py-2 px-4 rounded-md mb-2">
+    <div class="p-2">
+        <ul class="flex gap-4 bg-[#f8f8f8] rounded-md p-1 w-max overflow-hidden relative">
+            <!-- Tabs -->
+            <li>
+                <button id="allTab" class="tab text-[#0455b7] bg-white rounded-lg font-semibold text-center text-sm py-2 px-4 tracking-wide cursor-pointer">
+                    Tous les order misson
+                </button>
+            </li>
+            <li>
+                <button id="nouveauTab" class="tab text-gray-600 rounded-xl font-semibold text-center text-sm py-2 px-4 tracking-wide cursor-pointer">
+                    Nouveau
+                </button>
+            </li>
+           
+        </ul>
+    </div>
+    <div class="flex items-center justify-between">
+
+
     <a id="new" href="gerer_les_ordres_des_missions/order_mission_create.php" class="load-page-link flex items-center p-2 rounded-lg text-white bg-[#0455b7] transition-colors duration-300 transform hover:bg-blue-900">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="1.5">
             <path d="M12 5l0 14"></path>
@@ -43,34 +62,66 @@ $admin = $select->fetch(PDO::FETCH_ASSOC);
         </svg>
         <span class="mx-2 text-sm font-medium">create un order de misson</span>
     </a>
-    <div class="search-bar">
-        <input type="text" id="search-input" placeholder="Search order missions...">
-        <button id="search-button">Search</button>
     </div>
-    <table>
-            <tr>
-                <th>Order Number</th>
-                <th>Direction</th>
-                <th>Destination</th>
-                <th>Motif</th>
-                <th>Moyen de transport</th>
-                <th>Date de depart</th>
-                <th>Date de retour</th>
-                <th>Technicien</th>
-                <th>Actions</th>
+</div>
+
+    <div class="w-full bg-white flex flex-col items-center py-2 px-4 rounded-md">
+    <!-- search bar-->
+    <div class="w-full py-2 px-4 flex justify-between items-center">
+        <div class="flex space-x-2">
+            <div class="relative search-bar">
+                <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </div>
+                <input type="text" id="search-input" class="pl-10 pr-4 py-2 border border-gray-300 rounded-lg text-sm w-64" placeholder="Search order missions...">
+                <button type="submit" id="search-button" class="text-white bg-[#0455b7] transition-colors duration-300 transform hover:bg-blue-900 font-medium rounded-lg text-sm px-6 py-2 text-center  cursor-pointer">Search</button>
+            </div>
+        </div>
+        <button class="flex items-center px-3 py-2 border cursor-pointer border-gray-300 rounded-lg text-sm text-gray-600 transition-colors duration-300 transform hover:bg-[#c8d3f659] hover:text-[#0455b7]">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="1.5">
+                <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                <path d="M9 15h6"></path>
+                <path d="M12.5 17.5l2.5 -2.5l-2.5 -2.5"></path>
+            </svg>
+            <span class="mx-2 text-sm font-medium">Exporter</span>
+        </button>
+    </div>
+    <!-- /search bar -->
+    <div class="w-full">
+        <table  class="w-full divide-y divide-gray-200">
+        <tr class="tr-head">
+                <th scope="col" class="pl-4 w-[3%]">
+                    <input type="checkbox" name="select-rapport" id="select-all">
+                </th>
+                
+                <th cope="col" class="th-class" >Order Number</th>
+                <th cope="col" class="th-class" >Direction</th>
+                <th cope="col" class="th-class" >Destination</th>
+                <th cope="col" class="th-class" >Motif</th>
+                <th cope="col" class="th-class" >Moyen de transport</th>
+                <th cope="col" class="th-class" >Date de depart</th>
+                <th cope="col" class="th-class" >Date de retour</th>
+                <th cope="col" class="th-class" >Technicien</th>
+                <th cope="col" class="th-class" >Actions</th>
             </tr>
-        <div id="order-missions-table">
+            <div id="order-missions-table">
             <?php if (!empty($order_missions)) : ?>
                 <?php foreach ($order_missions as $order_mission) : ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($order_mission['order_num']); ?></td>
-                        <td><?php echo htmlspecialchars($order_mission['direction']); ?></td>
-                        <td><?php echo htmlspecialchars($order_mission['destination']); ?></td>
-                        <td><?php echo htmlspecialchars($order_mission['motif']); ?></td>
-                        <td><?php echo htmlspecialchars($order_mission['moyen_tr']); ?></td>
-                        <td><?php echo htmlspecialchars($order_mission['date_depart']); ?></td>
-                        <td><?php echo htmlspecialchars($order_mission['date_retour']); ?></td>
-                        <td>
+                    <tr class="tr-body">
+                    <td class="pl-4">
+                        <input type="checkbox" name="select-order" id="select-order-<?php echo htmlspecialchars($order_mission['order_num'])?? '';  ?>">
+                    </td>
+                        <td class="td-class"><?php echo htmlspecialchars($order_mission['order_num']); ?></td>
+                        <td class="td-class"><?php echo htmlspecialchars($order_mission['direction']); ?></td>
+                        <td class="td-class"><?php echo htmlspecialchars($order_mission['destination']); ?></td>
+                        <td class="td-class"><?php echo htmlspecialchars($order_mission['motif']); ?></td>
+                        <td class="td-class"><?php echo htmlspecialchars($order_mission['moyen_tr']); ?></td>
+                        <td class="td-class"><?php echo htmlspecialchars($order_mission['date_depart']); ?></td>
+                        <td class="td-class"><?php echo htmlspecialchars($order_mission['date_retour']); ?></td>
+                        <td class="td-class">
                             <?php
                             $technicien_id = $order_mission['technicien_id'];
                             $select = $conn->prepare("SELECT nom, prenom FROM Users WHERE user_id = ?");
@@ -79,11 +130,27 @@ $admin = $select->fetch(PDO::FETCH_ASSOC);
                             echo htmlspecialchars($technicien['nom'] . ' ' . $technicien['prenom']);
                             ?>
                         </td>
-                        <td>
+                        
+                        <td class="px-6 py-4 w-[3%] whitespace-nowrap text-sm font-medium">
+                        <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50" type="button">
+                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
+                                <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
+                            </svg>
+                        </button>
+                        <!-- Dropdown menu -->
+                        <div id="dropdownDots" class="absolute z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">
+                            <div class="py-2">
                             <a href="gerer_les_ordres_des_missions/order_mission_view.php?order_num=<?php echo $order_mission['order_num']; ?>" class="view-button">View</a>
+                            </div>
+                            <div class="py-2">
                             <a href="gerer_les_ordres_des_missions/order_mission_edit.php?order_num=<?php echo $order_mission['order_num']; ?>" class="edit-button">Edit</a>
+                            </div>
+                            <div class="py-2">
                             <a href="gerer_les_ordres_des_missions/order_mission_delete.php?order_num=<?php echo $order_mission['order_num']; ?>" class="delete-button">Delete</a>
-                        </td>
+                            </div>
+                        </div>
+                        <!-- /Dropdown menu -->
+                    </td>
                     </tr>
                 <?php endforeach; ?>
             <?php else : ?>
@@ -92,7 +159,9 @@ $admin = $select->fetch(PDO::FETCH_ASSOC);
                 </tr>
             <?php endif; ?>
         </div>
-    </table>
+        </table>
+    </div>
+</div>
 </section>
 
 <script>
--- dist/gerer_les_ordres_des_missions/order_mission_create.php
+++ dist/gerer_les_ordres_des_missions/order_mission_create.php
@@ -28,38 +28,82 @@ $techniciens = $conn->query("SELECT u.user_id, u.username, u.nom, u.prenom, r.ro
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
     <section class="form-container">
-        <h1>Create Order Mission</h1>
-        <form action="../app/order_mission_create.php" method="post">
-            <label for="direction">Direction:</label>
-            <input type="text" id="direction" name="direction" required>
+    <div class="bg-white border-gray-200 border-2 rounded-lg shadow-sm mx-0 ">
+        <!-- Title Form -->
+        <div class="flex items-start justify-between p-5 border-b">
+            <h3 class="text-xl font-bold text-gray-700">Create Order Mission</h3>
+            <!-- <img src="../../assets/image/oie_M5SLKyrEbkDJ.png" alt="Logo" class="h-10 w-auto "> -->
+        </div>
 
-            <label for="destination">Destination:</label>
-            <input type="text" id="destination" name="destination" required>
-
-            <label for="motif">Motif:</label>
-            <textarea id="motif" name="motif" required></textarea>
-
-            <label for="moyen_tr">Moyen de transport:</label>
-            <input type="text" id="moyen_tr" name="moyen_tr" required>
-
-            <label for="date_depart">Date de depart:</label>
-            <input type="date" id="date_depart" name="date_depart" required>
-
-            <label for="date_retour">Date de retour:</label>
-            <input type="date" id="date_retour" name="date_retour" required>
-
-            <label for="technicien_id">Technicien:</label>
-            <select id="technicien_id" name="technicien_id" required>
+        <div class="p-6 space-y-6">
+            <form action="../app/order_mission_create.php" method="post">
+                <div class="grid grid-cols-6 gap-5">
+                    
+                    <!-- selected Technicien -->
+                    <div class="col-span-6 sm:col-span-3">
+                    <label for="technicien_id" class="text-sm font-medium text-gray-900 block mb-2" >Technicien:</label>
+                    <select id="technicien_id" name="technicien_id" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" required>
                 <?php foreach ($techniciens as $technicien) : ?>
                     <option value="<?php echo $technicien['user_id']; ?>"><?php echo $technicien['nom'] . ' ' . $technicien['prenom']; ?></option>
                 <?php endforeach; ?>
-            </select>
+                </select>
+                    </div>
 
-            <button type="submit">Create Order Mission</button>
-        </form>
+                    <!-- Moyen de transport -->
+                    <div class="col-span-6 sm:col-span-3">
+                    <label for="moyen_tr" class="text-sm font-medium text-gray-900 block mb-2" >Moyen de transport:</label>
+            <input type="text" id="moyen_tr" name="moyen_tr" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-600 w-full p-2.5" placeholder="Moyen de transport" required>
+                    </div>
+
+                    <!-- Direction -->
+                    <div class="col-span-6 sm:col-span-3">
+                    <label for="direction" class="text-sm font-medium text-gray-900 block mb-2">Direction:</label>
+                    <input type="text" id="direction" name="direction" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-600 w-full p-2.5" placeholder="Direction:" required>
+                    </div>
+
+                    <!-- Destination -->
+                    <div class="col-span-6 sm:col-span-3">
+                    <label for="destination"  class="text-sm font-medium text-gray-900 block mb-2">Destination:</label>
+                    <input type="text" id="destination" name="destination" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-600 w-full p-2.5" placeholder="Destination" required><br><br>
+                    </div>
+
+                    
+                    <!-- Date de départ -->
+                    <div   div class="col-span-2 sm:col-span-3">
+                    <label for="date_depart" class="text-sm font-medium text-gray-900 block mb-2">📅 Date de départ:</label>
+                    <input type="date" id="date_depart" name="date_depart" class="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600  focus:border-cyan-600 hover:border-cyan-500 transition-all duration-200 ease-in-out 
+                    block w-full p-2.5  cursor-pointer"
+                    required>
+                    </div>
+
+                    <!-- Date de retour -->
+                    <div   div class="col-span-2 sm:col-span-3">
+                    <label for="date_retour" class="text-sm font-medium text-gray-900 block mb-2">📅 Date de retour:</label>
+                    <input type="date" id="date_retour" name="date_retour" class="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600  focus:border-cyan-600 hover:border-cyan-500 transition-all duration-200 ease-in-out 
+                    block w-full p-2.5  cursor-pointer"
+                    required>
+                    </div>
+
+
+                    <!-- Motif -->
+                    <div class="col-span-6 sm:col-span-6">
+                    <label for="motif" class="text-sm font-medium text-gray-900 block mb-2">Motif:</label>
+                    <textarea id="motif" name="motif" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5" required></textarea>
+                    </div>
+
+                </div>
+
+                <!-- Button Create Order Mission-->
+                <div class="p-6 pb-0 order-gray-200 rounded-b">
+                    <!-- <input type="submit" name="create_account" value="Create Order Mission" class="text-white bg-[#0455b7] transition-colors duration-300 transform hover:bg-blue-900 font-medium rounded-lg text-sm px-5 py-3 text-center"> -->
+                    <button type="submit" name="create_account"  class="text-white cursor-pointer bg-[#0455b7] transition-colors duration-300 transform hover:bg-blue-900 font-medium rounded-lg text-sm px-5 py-3 text-center">Create Order Mission</button>
+                </div>
+            </form>
+        </div>
+    </div>
     </section>
+
 
 </body>
 </html>
